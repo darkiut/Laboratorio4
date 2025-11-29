57063694c546101c5ffde2fb196fc4b65a589df4
darkiut <167654887+darkiut@users.noreply.github.com>
2025-11-29 12:23:11 -0500

    Update MainActivity.kt

--- app/src/main/java/com/negocio/lab04/MainActivity.kt
+++ app/src/main/java/com/negocio/lab04/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        Text(text = "Dev: CAMBIO DESDE GITHUB"),
         modifier = modifier
     )
 }
